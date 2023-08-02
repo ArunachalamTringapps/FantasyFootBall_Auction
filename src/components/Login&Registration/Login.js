0ad406547759b2eb04f3e395346904011d08eec0
subhi-29 <subhiksha.b@tringapps.com>
2023-08-02 14:10:07 +0530

    Login page

--- src/components/Login&Registration/Login.js
+++ src/components/Login&Registration/Login.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import "../../css/Login&Registrationcss/Login.css"
+// import { Link } from 'react-router-dom'
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='Login'>
+      <div className='container'>
+        <div className='left'>
+          hii
+        </div>
+        <div className='header'>
+          Login
+        <form className='form'>
+          <input type="text" placeholder='enter username'></input>
+          <input type="text" placeholder='enter email id'></input>
+          <input type="text" placeholder='enter password'></input>
+          <button className='signin'>Sign in</button>
+        </form>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
