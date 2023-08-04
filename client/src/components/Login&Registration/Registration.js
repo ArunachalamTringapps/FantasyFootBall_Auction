914dd925b0aa265d7f92dea7c261ce1af4610eb5
subhi-29 <subhiksha.b@tringapps.com>
2023-08-04 18:43:54 +0530

    registration completed

--- client/src/components/Login&Registration/Registration.js
+++ client/src/components/Login&Registration/Registration.js
@@ -1,83 +1,84 @@
 import React from 'react'
 import "../../css/Login&Registrationcss/Registration.css"
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Registration() {
-  const navigate=useNavigate()
-  const[email_id,setEmail_id]=useState('')
-  const[password_user,setPassword_user]=useState('')
-  const [data, setData] = useState([]);
+  const navigate = useNavigate()
+  const [email_id, setEmail_id] = useState('')
+  const [password_user, setPassword_user] = useState('')
   const [error, setError] = useState(null);
   const [phone_no, setPhone_no] = useState('');
   const [retypePassword, setRetypePassword] = useState('');
- 
+
   const handleRegister = async (e) => {
-     e.preventDefault();
+    e.preventDefault();
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!email_id){
-      setError("Email is required")
+    if (!email_id) {
+      toast.error("Email is required")
       setTimeout(() => setError(''), 3000);
       return
     }
     if (!emailPattern.test(email_id)) {
-      setError('Email should be in the correct format.');
+      toast.error('Email should be in the correct format.');
       setTimeout(() => setError(''), 3000);
       return;
     }
-    
+
     const phonePattern = /^\d{10}$/;
     if (!phone_no) {
-      setError("Phone number is required");
+      toast.error("Phone number is required");
       setTimeout(() => setError(''), 3000);
       return;
     }
     if (!phonePattern.test(phone_no)) {
-      setError('Phone number should be exactly 10 digits.');
+      toast.error('Phone number should be exactly 10 digits.');
       setTimeout(() => setError(''), 3000);
       return;
     }
 
     // Validate password format using regular expression
-    if (!password_user){
-      setError('Password is required')
+    if (!password_user) {
+      toast.error('Password is required')
       setTimeout(() => setError(''), 3000);
       return
-      
+
     }
     const passwordPattern = /^(?=.*[A-Za-z0-9@])[A-Za-z0-9@]{6}$/;
-    if(!passwordPattern.test(password_user)) {
-      setError('Password should have 6 alphanumeric characters.');
+    if (!passwordPattern.test(password_user)) {
+      toast.error('Password should have 6 alphanumeric characters.');
       setTimeout(() => setError(''), 3000);
       return
     }
-    if(password_user.length>6){
-      setError('Password must not exceed 6 alphanumeric characters')
+    if (password_user.length > 6) {
+      toast.error('Password must not exceed 6 alphanumeric characters')
       setTimeout(() => {
         setError('')
-      },3000);
+      }, 3000);
     }
     if (!retypePassword) {
-      setError("Retype password is required");
+      toast.error("Retype password is required");
       setTimeout(() => setError(''), 3000);
       return;
     }
-    if(password_user!=retypePassword){
-      setError('Password do not match Retype the correct password')
-      setTimeout(()=>{
+    if (password_user !==retypePassword) {
+    toast.error('Password do not match Retype the correct password')
+      setTimeout(() => {
         setError('')
-      },3000)
+      }, 3000)
       return;
     }
-    
-    
+
+
     try {
       const response = await fetch('http://localhost:5000/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email_id, password_user}),
+        body: JSON.stringify({ email_id, password_user }),
       });
 
       const data = await response.json();
@@ -91,7 +92,7 @@ function Registration() {
         setError(data.error);
         setTimeout(() => setError(''), 5000);
       }
-    } 
+    }
     catch (error) {
       console.error('Error:', error);
       setError('An error occurred during registration.');
@@ -102,27 +103,43 @@ function Registration() {
 
   return (
     <div className='Registration'>
-    <div className='container-registration'>
-      <div className='left-container'>
-      
+      <div className='container-registration'>
+        <div className='register-container'>
+        <h4> Registration</h4>
+          <form className='form-register' onSubmit={handleRegister} autoComplete='off'>
+            <div className='input-fields'>
+              <input type="text"  value={email_id} className='all-input' onChange={(e) => setEmail_id(e.target.value)}></input>
+              <label for="fields" className="label-fields">
+                <span className='content-fields'>Email_id...</span>
+              </label>
+            </div>
+            <div className='input-fields'>
+            <input type="text"  className='all-input' onChange={(e) => setPhone_no(e.target.value)}></input>
+            <label for="fields" className="label-fields">
+                <span className='content-fields'>Phone_no...</span>
+              </label>
+            </div>
+            <div className='input-fields'>
+            <input type="password"  value={password_user} className='all-input' onChange={(e) => setPassword_user(e.target.value)}></input>
+            <label for="fields" className="label-fields">
+                <span className='content-fields'>Password...</span>
+              </label>
+            </div>
+            <div className='input-fields'>
+            <input type="password" className='all-input' onChange={(e) => setRetypePassword(e.target.value)}></input>
+            <label for="fields" className="label-fields">
+                <span className='content-fields'>Retype_password...</span>
+              </label>
+            </div>
+            <div><button className='signup'>Sign up</button></div>
+          </form>
+          <div className='exist-account'>Already have an Account? <Link to="/login" className='link'>Login</Link></div>
+        </div>
+        <div className='header-register'>
+        </div>
       </div>
-      <div className='header-register'>
-        Registration
-      <form className='form' onSubmit={handleRegister}>
-        <input type="text" placeholder='email...' value={email_id} className='email' onChange={(e)=>setEmail_id(e.target.value)}></input>
-        <input type="number" placeholder='phoneno...' className='email' onChange={(e) => setPhone_no(e.target.value)}></input>
-        <input type="password" placeholder='password...'  value={password_user}className='password' onChange={(e)=>setPassword_user(e.target.value)}></input>
-        <input type="password" placeholder='Retype_password...' className='email' onChange={(e)=>setRetypePassword(e.target.value)}></input>
-        <div className='error'>{error && <p>{error}</p>}</div>
-       <div><button className='signin'>Sign in</button></div>
-      </form>
-      <div className='createaccount'>Already have an Account? <Link to="/login">Login</Link></div>
-      </div>
-      
-
+      <ToastContainer limit={1} position={'top-right'} pauseOnHover={false} pauseOnFocusLoss={false} draggable={false} closeOnClick={false}/>
     </div>
-    
-  </div>
 
   )
 }
