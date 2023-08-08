5364a410cf13b8b6f8917933bc2f506c2d1c7cc4
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-08-08 16:16:30 +0530

    updating create auction

--- client/src/components/Dashboard/CreateAuction.js
+++ client/src/components/Dashboard/CreateAuction.js
@@ -1,139 +1,35 @@
 import React,{useState} from 'react'
 import '../../css/Dashboardcss/CreateAuction.css'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 
 function CreateAuction() {
-  // const [date, setDate] = useState();
-    const [date, setDate] = useState(null); // Changed initial state to null
-  const [calendarOpen, setCalendarOpen] = useState(false);
-
-  const handleDateInputClick = () => {
-    setCalendarOpen(true);
-  };
   return (
     <div className='createAuction'>
-      <div className='Container'>
-         <form className='form_createAuction'>  
+        <form className='form_createAuction'>  
         <div className='inputbox'>
-            <input type="text" name="Auction_id" className='all-fields' ></input>
-            <label for="email" class="label-name">
-              <span className='content-name'>Auction_id...</span>
-            </label>
-        </div>
-        <div className='inputbox'>
-            <input type="text" name="Auction Name" className='all-fields' ></input>
-            <label for="email" class="label-name">
-              <span className='content-name'>Auction Name...</span>
-            </label>
-        </div>
-        {/* <div className='inputbox Date'>
+          <label>AUCTION NAME</label>
+          <input type="text" name="Auction Name" placeholder='Auction Name'className='fields' ></input>
 
-            <input type="date" name="Auction Date" value="" className='all-fields'></input> 
-            <DatePicker placeholderText=""  className='all-fields' />
-            <DatePicker selected={date} className='all-fields' onChange={(date) => setDate(date)} />
-
-            <label for="email" class="label-name">
-              <span className='content-name'>Auction Date...</span>
-            </label>
-        </div> */}
-        <div className='inputbox Date'>
-  
-            <div
-              className='all-fields'
-              onClick={handleDateInputClick}
-            >
-              {date ? date.toDateString() : ''}
-            </div>
-            <label htmlFor="email" className="label-name">
-              <span className='content-name'>Auction Date...</span>
-            </label>
-            {calendarOpen && (
-              <DatePicker
-                selected={date}
-                onChange={newDate => {
-                  setDate(newDate);
-                  setCalendarOpen(false);
-                }}
-                onClickOutside={() => setCalendarOpen(false)}
-                className='date-picker-calendar'
-                inline
-              />
-            )}
-          </div>
-        <div className='inputbox'>
-            <input type="text" name="Points Per Team" className='all-fields' ></input>
-            <label for="email" class="label-name">
-              <span className='content-name'>Points Per Team...</span>
-            </label>
         </div>
         <div className='inputbox'>
-            <input type="text" name="Players Per Team" className='all-fields' ></input>
-            <label for="email" class="label-name">
-              <span className='content-name'>Players Per Team...</span>
-            </label>
+          <label>AUCTION DATE</label>
+          <input type="date" name="Auction Date" className='fields' ></input>
         </div>
         <div className='inputbox'>
-            <input type="email" name="Email" className='all-fields'></input>
-            <label for="email" class="label-name">
-              <span className='content-name'>Email_id...</span>
-            </label>
+          <label>POINTS PER TEAM</label>
+          <input type="text" name="Points Per Team" placeholder='Points Per Team'className='fields' ></input>
+        </div>
+        <div className='inputbox'>
+          <label>PLAYERS PER TEAM</label>
+            <input type="text" name="Players Per Team" placeholder='Players Per Team' className='fields' ></input>
+        </div>
+        <div className='inputbox'>
+          <label>EMAIL</label>
+            <input type="email" name="Email" placeholder='Email' className='fields'></input>
         </div>
         </form>
-      </div>
       <button className='bt'>Add Auction</button>
     </div>
   )
 }
-
-// export default CreateAuction
-// import React, { useState } from 'react';
-// import '../../css/Dashboardcss/CreateAuction.css';
-// import DatePicker from "react-datepicker";
-// import "react-datepicker/dist/react-datepicker.css";
-// function CreateAuction() {
-//   const [date, setDate] = useState(null); // Changed initial state to null
-//   const [calendarOpen, setCalendarOpen] = useState(false);
-
-//   const handleDateInputClick = () => {
-//     setCalendarOpen(true);
-//   };
-
-//   return (
-//     <div className='createAuction'>
-//       <div className='Container'>
-//         <form className='form_createAuction'>
-//           {/* ...Other input fields */}
-//           <div className='inputbox Date'>
-//             {/* Changed input field to div with click event */}
-//             <div
-//               className='all-fields'
-//               onClick={handleDateInputClick}
-//             >
-//               {date ? date.toDateString() : 'Select Auction Date'}
-//             </div>
-//             <label htmlFor="email" className="label-name">
-//               <span className='content-name'>Auction Date...</span>
-//             </label>
-//             {calendarOpen && (
-//               <DatePicker
-//                 selected={date}
-//                 onChange={newDate => {
-//                   setDate(newDate);
-//                   setCalendarOpen(false);
-//                 }}
-//                 onClickOutside={() => setCalendarOpen(false)}
-//                 className='date-picker-calendar'
-//                 inline // Display the calendar inline with the input
-//               />
-//             )}
-//           </div>
-//           {/* ...Other input fields */}
-//         </form>
-//       </div>
-//       <button className='bt'>Add Auction</button>
-//     </div>
-//   );
-// }
 
 export default CreateAuction;
