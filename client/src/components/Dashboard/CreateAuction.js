1f4cda7d14c100f5145a7778389b4aeef96b78ac
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-08-09 18:53:48 +0530

    create auction completed

--- client/src/components/Dashboard/CreateAuction.js
+++ client/src/components/Dashboard/CreateAuction.js
@@ -1,13 +1,61 @@
 import React,{useState} from 'react'
 import '../../css/Dashboardcss/CreateAuction.css'
+import axios from "axios"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function CreateAuction() {
-  const [AuctionName,setAuctionName]=useState('');
-  const[AuctionDate,setAuctionDate]=useState('');
-  const[Pointsperteam,setpointsperteam]=useState('');
-  const[Playersperteam,setPlayersperteam]=useState('');
+  const[auction_name,setAuctionName]=useState('');
+  const[auction_date,setAuctionDate]=useState('');
+  const[points_per_team,setpointsperteam]=useState('');
+  const[players_per_team,setPlayersperteam]=useState('');
+  const handleCreateauction = async (e) => {
+    e.preventDefault();
+    const email_id=localStorage.getItem("useremail")
+    const currentDate = new Date();
+    const inputDate = new Date(auction_date);
+    console.log("now date",currentDate.toISOString().split('T')[0]);
+    console.log("input date",inputDate);
+    if (!auction_name) {
+      toast.error("Auction Name is required");
+      return
+    }
+    if(!auction_date){
+      toast.error("Auction Date is required");
+      return 
+    }
+    if (inputDate.toISOString().split('T')[0] < currentDate.toISOString().split('T')[0]) {
+      toast.error("Auction Date should be current or upcoming date");
+      return
+    }
+    if (!points_per_team) {
+      toast.error("Points Per Team is required");
+      return
+    }
+    if (!players_per_team) {
+      toast.error("Players Per Team is required");
+      return
+    }
+    if(players_per_team>11){
+      toast.error("Players Per Team should be less than 12");
+      return
+    }
+
+
+    axios.post('http://localhost:5000/api/createauction',{auction_name,auction_date,points_per_team,players_per_team,email_id})
+    .then((auctionData)=>{
+      console.log(auctionData);
+    })
+    .catch((err)=>{
+      console.error('Error:', err);
+      toast.error('An error occurred during registration.');
+    })
+    e.target.reset();
+  }
+
+
   return (
     <div className='createAuction'>
-        <form className='form_createAuction'>
+        <form className='form_createAuction' onSubmit={handleCreateauction} autoComplete='off'>
         <h2>Create Auction</h2>
         <div className='inputbox'>
           <label className='label'>AUCTION NAME</label>
@@ -24,10 +72,11 @@ function CreateAuction() {
         </div>
         <div className='inputbox'>
           <label className='label'>PLAYERS PER TEAM</label>
-            <input type="text" name="Players Per Team" placeholder='Players Per Team' className='fields' onChange={(e) => setPlayersperteam(e.target.value)}></input>
+            <input type="text" name="Players Per Team" defaultValue={11} placeholder='Players Per Team' className='fields' onChange={(e) => setPlayersperteam(e.target.value)}></input>
         </div>
-        <button className='bt'>Add Auction</button>
+        <button className='bt'>Submit</button>
         </form> 
+        <ToastContainer limit={1} position={'top-right'} pauseOnHover={false} pauseOnFocusLoss={false} draggable={false} closeOnClick={false}/>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const {registerroute,loginroute,auctionroute,userdataroute,currentauctionroute,upcomingauctionroute,historyauctionroute,searchPlayersRoute,teamauctionroute,playerdetailsRoute,teamsJoinsPlayersRoute,topfiveplayersRoute}=require('./backend/Route')
+const {registerroute,loginroute,createauctionroute,userdataroute,currentauctionroute,upcomingauctionroute,historyauctionroute,searchPlayersRoute,teamauctionroute,playerdetailsRoute,teamsJoinsPlayersRoute,topfiveplayersRoute}=require('./backend/Route')
 
 const app = express();
 const port = 5000;
@@ -9,8 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/login',loginroute)
 app.use('/api/register',registerroute)
-app.use('/api/createauction',auctionroute)
-app.use('/api',userdataroute)
+app.use('/api',userdataroute,createauctionroute)
 app.use('/api/auction',currentauctionroute,upcomingauctionroute,historyauctionroute)
 app.use('/api/search',searchPlayersRoute)
 app.use("/api/details",playerdetailsRoute,teamauctionroute)
