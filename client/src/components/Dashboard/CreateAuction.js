4b683283de891e43ff48d47ca944555f5ac52b4a
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-08-09 14:53:59 +0530

    create auction component ui

--- client/src/components/Dashboard/CreateAuction.js
+++ client/src/components/Dashboard/CreateAuction.js
@@ -1,32 +1,33 @@
 import React,{useState} from 'react'
 import '../../css/Dashboardcss/CreateAuction.css'
 function CreateAuction() {
+  const [AuctionName,setAuctionName]=useState('');
+  const[AuctionDate,setAuctionDate]=useState('');
+  const[Pointsperteam,setpointsperteam]=useState('');
+  const[Playersperteam,setPlayersperteam]=useState('');
   return (
     <div className='createAuction'>
         <form className='form_createAuction'>
+        <h2>Create Auction</h2>
         <div className='inputbox'>
-          <label>AUCTION NAME</label>
-          <input type="text" name="Auction Name" placeholder='Auction Name'className='fields' ></input>
+          <label className='label'>AUCTION NAME</label>
+          <input type="text" name="Auction Name" placeholder='Auction Name'className='fields' onChange={(e) => setAuctionName(e.target.value)} ></input>
 
         </div>
         <div className='inputbox'>
-          <label>AUCTION DATE</label>
-          <input type="date" name="Auction Date" className='fields' ></input>
+          <label className='label'>AUCTION DATE</label>
+          <input type="date" name="Auction Date" className='fields' onChange={(e) => setAuctionDate(e.target.value)}></input>
         </div>
         <div className='inputbox'>
-          <label>POINTS PER TEAM</label>
-          <input type="text" name="Points Per Team" placeholder='Points Per Team'className='fields' ></input>
+          <label className='label'>POINTS PER TEAM</label>
+          <input type="text" name="Points Per Team" placeholder='Points Per Team'className='fields' onChange={(e) => setpointsperteam(e.target.value)}></input>
         </div>
         <div className='inputbox'>
-          <label>PLAYERS PER TEAM</label>
-            <input type="text" name="Players Per Team" placeholder='Players Per Team' className='fields' ></input>
+          <label className='label'>PLAYERS PER TEAM</label>
+            <input type="text" name="Players Per Team" placeholder='Players Per Team' className='fields' onChange={(e) => setPlayersperteam(e.target.value)}></input>
         </div>
-        <div className='inputbox'>
-          <label>EMAIL</label>
-            <input type="email" name="Email" placeholder='Email' className='fields'></input>
-        </div>
-        </form>
-      <button className='bt'>Add Auction</button>
+        <button className='bt'>Add Auction</button>
+        </form> 
     </div>
   )
 }
